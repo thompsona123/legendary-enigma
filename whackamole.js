6b93c7fdfa6a41b099043a069527b794a538a740
thompsona123 <45362949+thompsona123@users.noreply.github.com>
2019-04-08 12:43:23 -0400

    Finished gMole

--- whackamole.js
+++ whackamole.js
@@ -4,14 +4,32 @@ let score = 0;
 let goodMoleClicked;
 let badMoleClicked;
 let timer = 5;
+let grid = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+let boxClicked;
+let gMole;
+let bMole;
+let gameValue = true;
+let grandomx; 
+let grandomy;
+let grectxcoor; 
+let grectycoor; 
+
+function mousePressed(){
+  moleClicked();
+}
 
 function setup() {
   createCanvas(750, 750);
-  
-  
-  
-
+  grandomx = int(random(0,3));
+  grandomy = int(random(0,3));
+  grectxcoor = grandomx * 250;
+  grectycoor = grandomy * 250;
 }
+
 function draw(){
  
   background(255);
@@ -22,16 +40,18 @@ if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60
     timer --;
   
   }
-  
+ fill(0,0,0); 
 text("time: " + timer, width/2, height/2);
   if (timer == 0) {
     background(255);
+    fill(0,0,0);
     text("GAME OVER", width/2, height*0.5);
-   
+    gameValue = false;
   }
-  
+  if(timer != 0){
+  goodMole();
 }
-
+}
 
 function setupGame(){
 
@@ -79,10 +99,29 @@ function scoreAndTime(){
 }
 
 function moleClicked(){
+  for(i = 0; i < 3; i++){
+    for(j = 0; j < 3; j++){
+      if(int(mouseX / 250) == i && int(mouseY / 250) == j){
+        boxClicked = grid[i][j];
+        console.log(boxClicked);
+      }
+    }
+  }
+  newGoodMole();
 }
 
 function goodMole(){
+  gMole = grid[grandomx][grandomy];
+  fill(0, 255, 0);
+  rect(grectxcoor, grectycoor, 250, 250);
 }
 
-function badMole(){
+function newGoodMole(){
+  if(gMole == boxClicked){
+    grandomx = int(random(0,3));
+    grandomy = int(random(0,3));
+    grectxcoor = grandomx * 250;
+    grectycoor = grandomy * 250;
+  }
 }
+
